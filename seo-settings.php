3e3dff40e1630fbcc1837c6364c6cd4b6d5f887b
AlMo0 <vanna.biz.ua@gmail.com>
2018-06-16 23:18:14 +0300

    add files zipball_url

--- seo-settings.php
+++ seo-settings.php
@@ -15,15 +15,33 @@ License: A "Slug" license name e.g. GPL2
     published by the Free Software Foundation.
 */
 
-//if( ! class_exists( 'Smashing_Updater' ) ){
-//    include_once plugin_dir_path( __FILE__ ) . 'update.php';
-//}
-//$updater = new Smashing_Updater( __FILE__ );
-//$updater->set_username( 'Alexinger' );
-//$updater->set_repository( 'seo-settings' );
-//$updater->initialize();
+if( ! class_exists( 'Smashing_Updater' ) ){
+    include_once plugin_dir_path( __FILE__ ) . 'update.php';
+}
+$updater = new Smashing_Updater( __FILE__ );
+$updater->set_username( 'Alexinger' );
+$updater->set_repository( 'seo-settings' );
+$updater->initialize();
 
-include_once plugin_dir_path(__FILE__) . 'plugin.php';
+//include_once plugin_dir_path(__FILE__) . 'plugin.php';
+//include_once('updater.php');
+//
+//if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+//    $config = array(
+//        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+//        'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
+//        'api_url' => 'https://api.github.com/repos/username/repository-name', // the GitHub API url of your GitHub repo
+//        'raw_url' => 'https://raw.github.com/username/repository-name/master', // the GitHub raw url of your GitHub repo
+//        'github_url' => 'https://github.com/username/repository-name', // the GitHub url of your GitHub repo
+//        'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the GitHub repo
+//        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+//        'requires' => '3.0', // which version of WordPress does your plugin require?
+//        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+//        'readme' => 'README.md', // which file to use as the readme for the version number
+//        'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+//    );
+//    new WP_GitHub_Updater($config);
+//}
 
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
 function my_scripts_method() {
