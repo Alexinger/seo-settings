d6cc6166a368d00f552e3dd50b1e1dc5c66299e6
AlMo0 <vanna.biz.ua@gmail.com>
2018-06-25 00:11:27 +0300

    Delete comment function

--- seo-settings.php
+++ seo-settings.php
@@ -342,13 +342,13 @@ function create_shortcode_callback() {
 	wp_die();
 }
 
-//function exec_php($matches){
-//    eval('ob_start();'.$matches[1].'$inline_execute_output = ob_get_contents();ob_end_clean();');
-//    return $inline_execute_output;
-//}
-//function inline_php($content){
-//    $content = preg_replace_callback('/\[exeeec\]((.|\n)*?)\[\/exeeec\]/', 'exec_php', $content);
-//    $content = preg_replace('/\[exeeec off\]((.|\n)*?)\[\/exeeec\]/', '$1', $content);
-//    return $content;
-//}
-//add_filter('the_content', 'inline_php', 0);
+function exec_php($matches){
+    eval('ob_start();'.$matches[1].'$inline_execute_output = ob_get_contents();ob_end_clean();');
+    return $inline_execute_output;
+}
+function inline_php($content){
+    $content = preg_replace_callback('/\[exeeec\]((.|\n)*?)\[\/exeeec\]/', 'exec_php', $content);
+    $content = preg_replace('/\[exeeec off\]((.|\n)*?)\[\/exeeec\]/', '$1', $content);
+    return $content;
+}
+add_filter('the_content', 'inline_php', 0);
