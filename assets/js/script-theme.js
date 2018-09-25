cf6bc691c10c5ea87bdab35139abb2016e5e1782
Alexinger <vanna.biz.ua@gmail.com>
2018-09-25 10:09:07 +0300

    Small timeInterval

--- assets/js/script-theme.js
+++ assets/js/script-theme.js
@@ -5,5 +5,5 @@ jQuery(document).ready(function ($) {
     setInterval(function () {
         $('#shipping_method li>label[for="shipping_method_0_flat_rate2"]').text('Нужна доставка');
         $('#shipping_method li>label[for="shipping_method_0_local_pickup1"]').text('Самовывоз');
-    }, 1000);
+    }, 500);
 });
