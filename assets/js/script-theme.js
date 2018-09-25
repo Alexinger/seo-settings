5be35d8659ab951e4046ec0f019d21988c9ac78e
Alexinger <vanna.biz.ua@gmail.com>
2018-09-25 10:06:56 +0300

    Changer setTimeout for setInterval

--- assets/js/script-theme.js
+++ assets/js/script-theme.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
     $('#main-wrapper').addClass('row-fluid');
 
 
-    setTimeout(function () {
+    setInterval(function () {
         $('#shipping_method li>label[for="shipping_method_0_flat_rate2"]').text('Нужна доставка');
         $('#shipping_method li>label[for="shipping_method_0_local_pickup1"]').text('Самовывоз');
     }, 1000);
