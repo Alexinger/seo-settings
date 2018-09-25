b35a526ee9e0cb54e3bf08faaf9236df70bce2a0
Alexinger <vanna.biz.ua@gmail.com>
2018-09-25 10:05:23 +0300

    Change function shipping

--- assets/js/script-theme.js
+++ assets/js/script-theme.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function ($) {
     $('#main-wrapper').addClass('row-fluid');
 
 
-    $('#shipping_method li>label[for="shipping_method_0_flat_rate2"]').text('Нужна доставка');
-    $('#shipping_method li>label[for="shipping_method_0_local_pickup1"]').text('Самовывоз');
+    setTimeout(function () {
+        $('#shipping_method li>label[for="shipping_method_0_flat_rate2"]').text('Нужна доставка');
+        $('#shipping_method li>label[for="shipping_method_0_local_pickup1"]').text('Самовывоз');
+    }, 1000);
 });
