2052e1eee9418dded2a09c69febffb8c9a6f9b35
AlMo0 <vanna.biz.ua@gmail.com>
2019-06-25 14:01:34 +0300

    Remove word 'Dostavka' and 'Samovivoz'

--- assets/js/script-theme.js
+++ assets/js/script-theme.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function ($) {
     $('#main-wrapper').addClass('row-fluid');
 
 
-    setInterval(function () {
-        $('#shipping_method li:first>label').text('Нужна доставка');
-        $('#shipping_method li:last>label').text('Самовывоз');
-    }, 500);
+    // setInterval(function () {
+    //     $('#shipping_method li:first>label').text('Нужна доставка');
+    //     $('#shipping_method li:last>label').text('Самовывоз');
+    // }, 500);
 });
