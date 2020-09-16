094bdb4597e2abba50d264934de101857b7925eb
AlMo0 <vanna.biz.ua@gmail.com>
2020-09-16 19:46:01 +0300

    test: guttenberg - false

--- seo-settings.php
+++ seo-settings.php
@@ -492,3 +492,20 @@ function inline_php($content)
 }
 add_filter('the_content', 'inline_php', 0);
 
+## Отключает Гутенберг (новый редактор блоков в WordPress).
+## ver: 1.2
+if( 'disable_gutenberg' ){
+    remove_theme_support( 'core-block-patterns' ); // WP 5.5
+
+    add_filter( 'use_block_editor_for_post_type', '__return_false', 100 );
+
+    // отключим подключение базовых css стилей для блоков
+    // ВАЖНО! когда выйдут виджеты на блоках или что-то еще, эту строку нужно будет комментировать
+    remove_action( 'wp_enqueue_scripts', 'wp_common_block_scripts_and_styles' );
+
+    // Move the Privacy Policy help notice back under the title field.
+    add_action( 'admin_init', function(){
+        remove_action( 'admin_notices', [ 'WP_Privacy_Policy_Content', 'notice' ] );
+        add_action( 'edit_form_after_title', [ 'WP_Privacy_Policy_Content', 'notice' ] );
+    } );
+}
