7dd7d1f3ab840b0117e9ce9121b26dcafcb10709
AlMo0 <vanna.biz.ua@gmail.com>
2020-10-03 10:52:20 +0300

    Comments code search h1

--- assets/js/script-theme.js
+++ assets/js/script-theme.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
     $("#main-wrapper").addClass("row-fluid");
 
-    $('h1').replaceWith(function(){
-        return $("<h2>", {html: $(this).html()}, "</h2>");
-    });
+    // $('h1').replaceWith(function(){
+    //     return $("<h2>", {html: $(this).html()}, "</h2>");
+    // });
 });
