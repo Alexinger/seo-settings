93c55cceb6e8cd434f072e914efe83f76135eaa4
AlMo0 <vanna.biz.ua@gmail.com>
2020-11-16 12:07:19 +0200

    Create few pages 'post', 'page'

--- inc/tabs-home-post-seo.php
+++ inc/tabs-home-post-seo.php
@@ -1,5 +1,12 @@
 <!-- Table all posts -->
+<?php
 
+global $post;
+
+$args = array('post_type' => array('post', 'attachment'), 'numberposts' => -1, 'nopaging' => true, 'post_mine_type' => 'image', 'post_status' => 'publish', 'order' => 'ASC', 'orderby' => 'title');
+$myposts = get_posts($args);
+
+?>
 <div class="mt-4">
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h2>Все посты сайта (publish)</h2>
@@ -9,30 +16,27 @@
         <thead class="blue-grey white-text">
         <tr class="text-center text-uppercase">
             <th scope="col">ID</th>
-            <th scope="col">Ссылка постов</th>
-            <th scope="col">Название поста</th>
+            <th scope="col">Ссылка на пост</th>
+            <th scope="col">Описание ошибки или проблемы</th>
             <th scope="col">Ошибки</th>
-            <th scope="col">Проблема</th>
         </tr>
         </thead>
         <tbody>
 
         <?php
-        $pages = get_posts();
-        foreach( $pages as $page ){
-            if($page->post_status === 'publish'){
-                echo '<tr>
-                        <th scope="row" class="text-center">'. $page->ID .'</th>
-                        <td>' . get_page_link( $page->ID ) . '</td>
-                        <td>' . esc_html($page->post_title) . '</td>
-                        <td class="text-center">
-                            <span class="fa fa-close text-danger checkbox-result_' . $page->ID . '"></span>
-                        </td>
-                        <td class="text-center"><span id="' . $page->ID . '" class="btn btn-sm rgba-green-strong btnSaveCheck">Изменить</span></td>
-                      </tr>';
-            }
-        }
+        foreach ($myposts as $post) {
+        setup_postdata($post);
         ?>
+        <tr>
+            <th scope="row" class="text-center idProduct" style="vertical-align: middle;"><?php print $post->ID; ?></th>
+            <td><a href="<?php the_permalink(); ?>" target="_blank"><?php the_title(); ?></a></td>
+            <td class="w-50"><div id="<?php print $post->ID; ?>" class="editFieldsTasks bg-light contenteditable" contenteditable="true">Здесь можно писать</div></td>
+            <td class="text-center" style="vertical-align: middle;">
+                <span class="fa fa-close text-danger checkbox-result_<?php print $post->ID; ?>"></span>
+            </td>
+
+        </tr>
+        <?php } ?>
 
         </tbody>
     </table>
