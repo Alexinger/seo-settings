acaf98933afa0041f58f79eab911e53314a3d5b9
AlMo0 <vanna.biz.ua@gmail.com>
2020-12-21 16:58:39 +0200

    Remove style import in theme 'stripperd-down-bootstrap'

--- inc/tabs-extra-cron.php
+++ inc/tabs-extra-cron.php
@@ -2,7 +2,32 @@
 
 <button class="btn btn-dark-green rounded">Проверить</button>
 
+<form enctype="multipart/form-data" action="" method="POST">
+    <?php wp_nonce_field( 'my_file_upload', 'fileup_nonce' ); ?>
+    <input name="my_file_upload" type="file" />
+    <input type="submit" value="Загрузить файл" />
+</form>
+
 <?php
+
+if( wp_verify_nonce( $_POST['fileup_nonce'], 'my_file_upload' ) ){
+
+    if ( ! function_exists( 'wp_handle_upload' ) )
+        require_once( ABSPATH . 'wp-admin/includes/file.php' );
+
+    $file = & $_FILES['my_file_upload'];
+
+    $overrides = [ 'test_form' => false ];
+
+    $movefile = wp_handle_upload( $file, $overrides );
+
+    if ( $movefile && empty($movefile['error']) ) {
+        echo "Файл был успешно загружен.\n";
+        print_r( $movefile );
+    } else {
+        echo "Возможны атаки при загрузке файла!\n";
+    }
+}
 
 mb_internal_encoding("UTF-8");
 define('TELEGRAM_TOKEN', '1384324426:AAHIMNA_wPUBEfabuTbBDjHWODyH9AqshGs');
