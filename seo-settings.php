99738c2c986a143b20a55de717b623bd6e373cbe
Alexinger <alex777anders@gmail.com>
2021-08-04 15:06:53 +0300

    Add file style and add imort

--- seo-settings.php
+++ seo-settings.php
@@ -31,6 +31,12 @@ function my_scripts_method()
     wp_enqueue_style('my_style');
 }
 
+// load css into the website's front-end
+function mytheme_enqueue_style() {
+    wp_enqueue_style( 'mytheme-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_style' );
+
 // Added counter Yandex in footer
 include_once 'counter/counter-yandex.php';
 
