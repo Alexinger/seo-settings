10ec497ec2929a453caacbd3c4d244e4f6ad5001
Alexandr <alex777anders@gmail.com>
2021-11-05 08:25:06 +0200

    fix last version plugin and added shortcode to canister

--- inc/googlesheet/UpdatePrice.php
+++ inc/googlesheet/UpdatePrice.php
@@ -2,7 +2,40 @@
 
 class UpdatePrice
 {
-    public static function update_get_option()
+    public function update_get_option()
+    {
+        $gid = get_option("tabs-shortcode-url");
+        $id = get_option("tabs-shortcode-page");
+        $csv = file_get_contents('https://docs.google.com/spreadsheets/d/' . $gid . '/export?format=csv&gid=' . $id);
+        $csv = explode("\r\n", $csv);
+        $array = array_map('str_getcsv', $csv);
+
+        /* All row item water - NEW */
+        for ($row = 1; $row < 15; $row++) {
+            /*[50] [151] [751] [1201] [2501] [?] [?] [?] [?] [?]*/
+            if(isset($array[$row][0])){
+                update_option($row . '_row_left', $array[$row][0]);
+            }
+        }
+
+        /* All column item temperature - (const) NEW */
+        for ($header = 1; $header < 15; $header++) {
+            // [-10] [-15] [-20] [-25] [-35] [-35] [-40] [?] [?] [?]
+            if(isset($array[0][$header])){
+                update_option($header . '_header', substr(isset($array[0][$header]), 1, 2));
+            }
+        }
+
+        /* -10C -15C -20C -25C -30C -35C -40C -45C -50C -55C (50-150) */
+        for ($s = 0; $s < 15; $s++) {
+            for ($i = 0; $i < 15; $i++) {
+                if(isset($array[$i][$s+1])){
+                    update_option($i . '_row_' . $s . '_header', $array[$i][$s + 1]);
+                }
+            }
+        }
+    }
+    public function update_get_option_canister()
     {
         $gid = get_option("tabs-shortcode-url");
         $id = get_option("tabs-shortcode-page");
--- inc/googlesheet/show-table-shortcode.php
+++ inc/googlesheet/show-table-shortcode.php
@@ -1,6 +1,5 @@
 <?php
 add_shortcode("google_sheet", "get_google_sheet");
-
 function get_google_sheet($atts = null)
 {
     $atts = shortcode_atts([
@@ -27,7 +26,7 @@ function get_google_sheet($atts = null)
            <thead style="color: ' . $atts['table_color_value'] . '">
             <tr>';
     for ($i = 0; $i < 16; $i++) {
-        if (isset($array[0][$i])) {
+        if (isset($array[0][$i]) && $array[0][$i] != '') {
             $start .= '<th class="my-table-th">' . $array[0][$i] . '</th>';
         }
     }
@@ -47,4 +46,52 @@ function get_google_sheet($atts = null)
     $end = '</table>';
 
     return $start . $tr1 . $tr2 . $tr3 . $tr4 . $tr5 . $tr6 . $tr7 . $tr8 . $tr9 . $tr10 . $tr11 . $tr12 . $tr13 . $tr14 . $tr15 . $end;
+}
+
+add_shortcode("google_sheet_canister", "get_google_sheet_canister");
+function get_google_sheet_canister($atts = null)
+{
+    $atts = shortcode_atts([
+        'title' => 'Прайс лист канистр',
+        'title_color' => '#a5bf8f',
+        'table_bg' => '#c9c9c9',
+        'table_color_value' => 'white',
+    ], $atts);
+
+    $gid = get_option("tabs-shortcode-url");
+    $id = get_option("tabs-shortcode-page");
+    $csv = file_get_contents('https://docs.google.com/spreadsheets/d/' . $gid . '/export?format=csv&gid=' . $id);
+    $csv = explode("\r\n", $csv);
+    $array = array_map('str_getcsv', $csv);
+
+    $updateCanister = new UpdatePrice();
+    $updateCanister->update_get_option_canister();
+
+    $tr21 = $tr22 = $tr23 = $tr24 = $tr25 = $tr26 = $tr27 = $tr28 = $tr29 = $tr30 = $tr31 = $tr32 = $tr33 = $tr34 = $tr35 = null;
+
+    $start = '<style>th.my-table { color: ' . $atts['table_color_value'] . ' !important;white-space: pre-wrap !important;vertical-align: middle;line-height: 0.7rem !important;}</style>' .
+        '<h3 style="margin-bottom: 20px; text-align: center;color: ' . $atts['title_color'] . ';">' . $atts['title'] . '</h3>' .
+        '<table class="table table-bordered table-hover table-my-style" style="background-color: ' . $atts['table_bg'] . '">
+           <thead style="color: ' . $atts['table_color_value'] . '">
+            <tr>';
+    for ($i = 0; $i < 10; $i++) {
+        if (isset($array[20][$i]) && $array[20][$i] !== '') {
+            $start .= '<th class="my-table-th">' . $array[20][$i] . '</th>';
+        }
+    }
+    $start .= '</tr></thead>';
+
+    for ($s = 21; $s < 35; $s++) {
+
+        ${'tr' . $s} .= '<tr>';
+        for ($i = 0; $i < 8; $i++) {
+            if (isset($array[$s][$i]) && $array[$s][$i] != '') {
+                ${'tr' . $s} .= '<td class="my-table">' . $array[$s][$i] . '</td>';
+            }
+        }
+        ${'tr' . $s} .= '</tr>';
+    }
+    $end = '</table>';
+
+    return $start . $tr21 . $tr22 . $tr23 . $tr24 . $tr25 . $tr26 . $tr27 . $tr28 . $tr29 . $tr30 . $tr31 . $tr32 . $tr33 . $tr34 . $tr35 . $end;
 }
