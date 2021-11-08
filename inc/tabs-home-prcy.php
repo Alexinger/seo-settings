13c394c8e67e7b56a4601cc862eb669485e988e5
Alexandr <alex777anders@gmail.com>
2021-11-08 20:37:19 +0200

    upload remove all files

--- inc/tabs-home-prcy.php
+++
@@ -1,6 +0,0 @@
-<?php
-$site = site_url();
-?>
-<div id="iframes" style="overflow: hidden" class="mt-3">
-    <iframe src="https://pr-cy.ru/speed_test/?device=desktop&url=<?php print $site; ?>" style="width: 101.1%;height:700px"></iframe>
-</div>
